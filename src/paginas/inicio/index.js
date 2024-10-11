9482e35fa0b189a520d30a02332665106abf6bb3
Thiago Vanzele <thiagogomes@wiseoffices.com.br>
2024-10-11 10:38:39 -0300

    feat: Posts area created

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,10 +1,21 @@
 import Banner from "components/Banner";
+import styles from "./Inicio.module.css";
+
+import posts from "json/posts.json";
+import Post from "components/Post";
 
 export default function Inicio() {
   return (
     <main>
       <Banner />
-      <h1>Olá mundo!</h1>
+
+      <ul className={styles.posts}>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Post post={post}/>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
